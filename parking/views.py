dc92e44f81814d11195b129620b0403425f5a68e
Renan Santos <renan.santos.oliveira.sa@gmail.com>
2025-05-12 21:53:09 -0300

    "filtros usando RQL"

--- parking/views.py
+++ parking/views.py
@@ -1,6 +1,8 @@
 from rest_framework.permissions import DjangoModelPermissions
 from rest_framework import viewsets
 
+from parking.filters import ParkingSpotFilterClass
+from parking.filters import ParkingRecordFilterClass
 from parking.models import ParkingRecord, ParkingSpot
 from parking.serializers import ParkingSpotSerializer, ParkingRecordSerializer
 
@@ -8,12 +10,14 @@ from parking.serializers import ParkingSpotSerializer, ParkingRecordSerializer
 class ParkingSpotViewSet(viewsets.ModelViewSet):
     queryset = ParkingSpot.objects.all()
     serializer_class = ParkingSpotSerializer
+    rql_filter_class = ParkingSpotFilterClass
     permission_classes = [DjangoModelPermissions]
 
 
 class ParkingRecordViewSet(viewsets.ModelViewSet):
     queryset = ParkingRecord.objects.all()
     serializer_class = ParkingRecordSerializer
+    rql_filter_class = ParkingRecordFilterClass
     permission_classes = [DjangoModelPermissions]
 
     def get_queryset(self):
